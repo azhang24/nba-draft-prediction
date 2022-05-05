bdb619bf1ce6e3bf82581a97b7eacb493c3639b4
Andrew Zhang <andrewyzhang24@gmail.com>
2022-05-05 00:00:57 -0400

    Retrieve general, per 40, per 100 poss, and advanced stat for any ncaa player
    **TODO**
    - Not every player url has "1" appended. Ex. the url for Anthony Davis is anthony-davis-5. Need to make a mapping for these special cases.
    - Gather NBA Draft prospects that played in the NCAA from 2011 onwards
    - Place data in a database (or JSON to start off) to speed up querying

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import player_api
+import json
+import argparse
 
 def main():
-    print(player_api.get_player_ncaa_stats("Chet Holmgren", "Per 40"))
+    parser = argparse.ArgumentParser(description="Get stats for NBA Draft Prospects in the NCAA")
+    parser.add_argument('--name', help='Name of NBA Draft Prospect in NCAA (ex. Chet Holmgren)')
+    parser.add_argument('--year', type=int, help='Year prospect declared for draft (cannot be earlier than 2011)')
+    args = parser.parse_args()
+    name = args.name
+    year = args.year
+    print("NCAA Stats for {}".format(name))
+    print(json.dumps(player_api.get_player_ncaa_stats(
+        player_name=name, 
+        stat_types=["General", "Per 40", "Per 100 Poss", "Advanced"], 
+        year=year), indent=4))
 
 if __name__ == "__main__":
     main()
