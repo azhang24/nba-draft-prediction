1b22b8bd4e3bcbc2bac32700b942d7a9d8f13afc
Andrew Zhang <andrewyzhang24@gmail.com>
2022-05-22 20:52:31 -0400

    Get peak win shares for NBA draft prospects, add more players to player id override mapping, refactored methods

--- get_player_win_shares_nba.py
+++ get_player_win_shares_nba.py
@@ -9,7 +9,8 @@ def main():
     parser.add_argument('--seasons', type=int, help='Number of seasons to total win shares for each player')
     parser.add_argument('--ncaa_only', help='Only retrieve win shares of players who played in the NCAA', action='store_true')
     parser.add_argument('--playoffs', help='Include playoff win shares', action='store_true')
-    parser.add_argument('--sorted', help='Sort players in Json for each year by decreasing total win shares', action='store_true')
+    parser.add_argument('--sort_total', help='Sort players in Json for each year by decreasing total win shares', action='store_true')
+    parser.add_argument('--sort_max', help='Sort players in Json for each year by decreasing max win shares', action='store_true')
 
     args = parser.parse_args()
     start_year = args.start_year
@@ -17,7 +18,8 @@ def main():
     num_seasons = args.seasons
     ncaa_only = args.ncaa_only
     playoffs = args.playoffs
-    is_sorted = args.sorted
+    sort_total = args.sort_total
+    sort_max = args.sort_max
 
     if end_year == None:
         end_year = 2021
@@ -28,29 +30,33 @@ def main():
         players[year] = []
         draft_class = player_api.get_draft_class(year=year, ncaa_only=ncaa_only)
         for player in draft_class:
-            player_win_shares = player_api.get_total_win_shares_nba(player_name=player, start_year=(year+1), num_seasons=num_seasons, playoffs=playoffs)
+            player_total_win_shares = player_api.get_total_win_shares_nba(player_name=player, draft_year=year, start_year=(year+1), num_seasons=num_seasons, playoffs=playoffs)
+            player_max_win_shares = player_api.get_max_win_shares_nba(player_name=player, draft_year=year, start_year=(year+1), num_seasons=num_seasons, playoffs=playoffs)
             player_stats = {}
             player_stats['player_name'] = player
-            player_stats['win_shares'] = player_win_shares
+            player_stats['total_win_shares'] = player_total_win_shares
+            player_stats['max_win_shares'] = player_max_win_shares
             players[year].append(player_stats)
-        if is_sorted:
-            players[year] = sort_by_win_shares(players[year])
+        if sort_total or sort_max:
+            key = None
+            if sort_total:
+                key = 'total_win_shares'
+            elif sort_max:
+                key = 'max_win_shares'
+            players[year] = sort_by_win_shares(players[year], key=key)
     
     f = open('player_win_shares.json', 'w')
     json.dump(players, f, ensure_ascii=False, indent=4)
 
-def sort_by_win_shares(players):
+def sort_by_win_shares(players, key):
     win_shares_to_player = {}
     for player in players:
-        win_shares_to_player[player['win_shares']] = player['player_name']
+        win_shares_to_player[player[key]] = player
     win_shares = win_shares_to_player.keys()
     win_shares_sorted = sorted(win_shares, reverse=True)
     sorted_players = []
     for win_share in win_shares_sorted:
-        player_info = {
-            'player_name': win_shares_to_player[win_share],
-            'win_shares': win_share
-        }
+        player_info = win_shares_to_player[win_share]
         sorted_players.append(player_info)
     return sorted_players
 
--- player_api.py
+++ player_api.py
@@ -3,8 +3,77 @@ import requests
 import unidecode
 
 exclude_stats = ['season', 'school_name', 'conf_abbr', 'sos-dum', 'ws-dum', 'poss-dum', 'bpm-dum']
-player_override_mapping = {
-    "Anthony Davis": "02"
+player_id_override_mapping = {
+    ("Derrick Williams", 2011): "02",
+    ("Brandon Knight", 2011): "03",
+    ("Kemba Walker", 2011): "02",
+    ("Markieff Morris", 2011): "02",
+    ("Marcus Morris", 2011): "03",
+    ("Tobias Harris", 2011): "02",
+    ("Jordan Hamilton", 2011): "02",
+    ("JaJuan Johnson", 2011): "02",
+    ("Bojan Bogdanović", 2011): "02",
+    ("Jordan Williams", 2011): "03",
+    ("Trey Thompkins", 2011): "02",
+    ("Keith Benson", 2011): "02",
+    ("Isaiah Thomas", 2011): "02",
+    ("Anthony Davis", 2012): "02",
+    ("Harrison Barnes", 2012): "02",
+    ("Royce White", 2012): "03",
+    ("Jeff Taylor", 2012): "03",
+    ("Darius Johnson-Odom", 2012): "03",
+    ("Tim Hardaway Jr.", 2013): "02",
+    ("Andre Roberson", 2013): "03",
+    ("Glen Rice Jr.", 2013): "02",
+    ("Tony Mitchell", 2013): "02",
+    ("P.J. Hairston", 2014): "02",
+    ("Glenn Robinson III", 2014): "02",
+    ("Markel Brown", 2014): "02",
+    ("Stanley Johnson", 2015): "04",
+    ("Jerian Grant", 2015): "02",
+    ("Larry Nance Jr.", 2015): "02",
+    ("Anthony Brown", 2015): "02",
+    ("Marcus Thornton", 2015): "02",
+    ("Dakari Johnson", 2015): "04",
+    ("Jaylen Brown", 2016): "02",
+    ("Taurean Prince", 2016): "02",
+    ("Brice Johnson", 2016): "02",
+    ("Damian Jones", 2016): "03",
+    ("Daniel Hamilton", 2016): "02",
+    ("Josh Jackson", 2017): "02",
+    ("Dennis Smith Jr.", 2017): "03",
+    ("Sterling Brown", 2017): "02",
+    ("Jaren Jackson Jr.", 2018): "02",
+    ("Miles Bridges", 2018): "02",
+    ("Robert Williams", 2018): "04",
+    ("Jacob Evans", 2018): "02",
+    ("Gary Trent Jr.", 2018): "02",
+    ("Justin Jackson", 2018): "02",
+    ("Alize Johnson", 2018): "02",
+    ("George King", 2018): "03",
+    ("Jaxson Hayes", 2019): "02",
+    ("Cameron Johnson", 2019): "02",
+    ("Keldon Johnson", 2019): "04",
+    ("Kevin Porter Jr.", 2019): "02",
+    ("Justin Wright-Foreman", 2019): "02",
+    ("Jalen Smith", 2020): "04",
+    ("Josh Green", 2020): "02",
+    ("Jaden McDaniels", 2020): "02",
+    ("Kenyon Martin Jr.", 2020): "04",
+    ("Jalen Green", 2021): "05",
+    ("Ziaire Williams", 2021): "02",
+    ("Trey Murphy III", 2021): "02",
+    ("Jalen Johnson", 2021): "05",
+    ("Keon Johnson", 2021): "07",
+    ("Cam Thomas", 2021): "02",
+    ("Jeremiah Robinson-Earl", 2021): "02",
+    ("Jared Butler", 2021): "02",
+    ("Kessler Edwards", 2021): "02",
+    ("David Johnson", 2021): "08"  
+}
+player_name_override_mapping = {
+    "Enes Freedom": "Enes Kanter",
+    "Nenê": "Nenê Hilário"
 }
 
 def get_player_ncaa_stats(player_name, stat_types, year):
@@ -55,31 +124,11 @@ def get_draft_class(year, ncaa_only=False):
                 players.append(player_name)
     return players
 
-def get_total_win_shares_nba(player_name, start_year=None, num_seasons=None, playoffs=False):
-    names = player_name.split()
-    first_name = names[0]
-    last_name = ""
-    if len(names) == 1:
-        if first_name == "Nenê":
-            last_name = "Hilário"
-    else:
-        last_name = names[1]
-    first_name = unidecode.unidecode(first_name.lower())
-    last_name = unidecode.unidecode(last_name.lower())
-    player_id = last_name[:5]+first_name[:2]
-    if player_name in player_override_mapping:
-        player_id = player_id + player_override_mapping[player_name]
-    else:
-        player_id = player_id + "01" 
-    url = "https://www.basketball-reference.com/players/{}/{}.html".format(last_name[0], player_id)
-    soup = get_soup(url)
+def get_total_win_shares_nba(player_name, draft_year, start_year=None, num_seasons=None, playoffs=False):
+    soup = get_player_soup(player_name, draft_year)
+    print(player_name)
     if num_seasons == None: # Get entire career
-        advanced_stats_table = soup.find(id='advanced')
-        all_seasons = advanced_stats_table.tbody.find_all(class_='full_table')
-        if start_year == None: # start year defaults to the rookie season
-            start_year = int(all_seasons[0].get('id').split('.')[1])
-        end_year = int(all_seasons[-1].get('id').split('.')[1])
-        num_seasons = end_year - start_year + 1
+        num_seasons = get_num_seasons(soup, start_year)
     total_ws = get_total_win_shares_from_soup(soup, start_year, num_seasons)
     if playoffs:
         total_ws += get_total_win_shares_from_soup(soup, start_year, num_seasons, playoffs=True)
@@ -88,24 +137,73 @@ def get_total_win_shares_nba(player_name, start_year=None, num_seasons=None, pla
 def get_total_win_shares_from_soup(soup, start_year, num_seasons, playoffs=False):
     total_ws = 0.0
     for year in range(start_year, start_year+num_seasons):
-        html_id = 'playoffs_advanced' if playoffs else 'advanced'
-        html_id = html_id + '.{}'.format(year)
-        advanced_stats_row = soup.find(id=html_id)
-        if advanced_stats_row != None:
-            advanced_stats_attributes = advanced_stats_row.find_all('td')
-            for attribute in advanced_stats_attributes:
-                if 'data-stat' in attribute.attrs and attribute['data-stat'] == 'ws':
-                    ws = attribute.contents[0]
-                    has_strong = attribute.find('strong')
-                    if has_strong != None:
-                        ws = attribute.strong.contents[0]
-                    total_ws += float(ws)
+        total_ws += get_win_shares_for_year(soup, year, playoffs)
     return total_ws
 
+def get_win_shares_for_year(soup, year, playoffs=False):
+    html_id = 'playoffs_advanced' if playoffs else 'advanced'
+    html_id = html_id + '.{}'.format(year)
+    advanced_stats_row = soup.find(id=html_id)
+    ws = 0.0
+    if advanced_stats_row != None:
+        advanced_stats_attributes = advanced_stats_row.find_all('td')
+        for attribute in advanced_stats_attributes:
+            if 'data-stat' in attribute.attrs and attribute['data-stat'] == 'ws':
+                ws = attribute.contents[0]
+                has_strong = attribute.find('strong')
+                if has_strong != None:
+                    ws = attribute.strong.contents[0]
+    return float(ws)
+
+def get_max_win_shares_nba(player_name, draft_year, start_year=None, num_seasons=None, playoffs=False):
+    soup = get_player_soup(player_name, draft_year)
+    if num_seasons == None: # Get entire career
+        num_seasons = get_num_seasons(soup, start_year)
+    max_ws = -10000000000.0
+    for year in range(start_year, start_year+num_seasons):
+        ws = get_win_shares_for_year(soup, year, playoffs=False)
+        playoff_ws = 0.0
+        if playoffs:
+            playoff_ws = get_win_shares_for_year(soup, year, playoffs=playoffs)
+        total_ws_for_year = ws + playoff_ws
+        if total_ws_for_year > max_ws:
+            max_ws = total_ws_for_year
+    if max_ws == -10000000000.0:
+        max_ws = 0.0
+    return max_ws
+
+def get_player_soup(player_name, draft_year):
+    if player_name in player_name_override_mapping:
+        player_name = player_name_override_mapping[player_name]
+    names = player_name.split()
+    first_name = names[0]
+    last_name = names[1]
+    first_name = unidecode.unidecode(first_name.lower())
+    last_name = unidecode.unidecode(last_name.lower())
+    first_name = first_name.replace("\'", "")
+    last_name = last_name.replace("\'", "")
+    player_id = last_name[:5]+first_name[:2]
+    if (player_name, draft_year) in player_id_override_mapping:
+        player_id = player_id + player_id_override_mapping[(player_name, draft_year)]
+    else:
+        player_id = player_id + "01" 
+    url = "https://www.basketball-reference.com/players/{}/{}.html".format(last_name[0], player_id)
+    return get_soup(url)
 
 def get_soup(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     return soup
+
+def get_num_seasons(soup, start_year=None):
+    advanced_stats_table = soup.find(id='advanced')
+    if advanced_stats_table == None:
+        return 0
+    all_seasons = advanced_stats_table.tbody.find_all(class_='full_table')
+    if start_year == None: # start year defaults to the rookie season
+        start_year = int(all_seasons[0].get('id').split('.')[1])
+    end_year = int(all_seasons[-1].get('id').split('.')[1])
+    num_seasons = end_year - start_year + 1
+    return num_seasons
 
 
